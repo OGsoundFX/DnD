3986deed05d22d038075596eb179ee80ef65df7c
Olivier Girardot <olive_girardot@hotmail.com>
2020-09-04 12:05:15 +0200

    improved the text display function. still lokking for a qay to chain functions

--- interaction.js
+++ interaction.js
@@ -1,22 +1,54 @@
 let i = 0;
 let speed = 50;
 const introText = 'You just woke up in the woods with a mighty headache! You have no recollection of how you ended up here. Yet you can not wait in this forest for your memory to come back, as you can sense danger lurking all around. While your head is pounding underneath your helmet, you gather your strength and your courage to start your quest and retrieve your identity!';
-// const introText = "hello";
+const hello = "hello";
 const enter = "Press enter";
 
 const askName = 'What is thy name?'
 
-const typeWriter = () => {
-  if (i < introText.length) {
-    document.getElementById("intro").innerHTML += introText.charAt(i);
+const typeWriter = (text) => {
+  if (i < text.length) {
+    document.getElementById("intro").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typeWriter, speed);
+      if (i === 50 || 80 || 100) {
+        setTimeout(typeWriter, speed, text);
+      } else {
+        setTimeout(typeWriter, speed, text);
+      };
   } else {
     setTimeout(function(){ document.querySelector(".text-font").innerHTML += `<p id='enter'>${enter}</p>`; }, 1500);
 
   };
 };
-typeWriter();
+// typeWriter(introText);
+
+class ChainAble {
+  firstMethod() {
+    console.log('This is the First Method');
+    return this;
+  }
+
+  secondMethod() {
+    console.log('This is the Second Method');
+    return this;
+  }
+
+  thirdMethod() {
+    console.log('This is the Third Method');
+    return this;
+  }
+}
+
+const chainableInstance = new Chainable()
+chainableInstance
+  .firstMethod()
+  .secondMethod()
+  .thirdMethod();
+
+// Console Output
+// This is the First Method
+// This is the Second Method
+// This is the Third Method
 
 
 // creating character
