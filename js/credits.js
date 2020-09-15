7369c6c7048578c3739be51cf59e1f35675ddaac
Olivier Girardot <olive_girardot@hotmail.com>
2020-09-15 21:02:15 +0200

    implemented fade out, awsome

--- js/credits.js
+++ js/credits.js
@@ -1,17 +1,4 @@
-// change page
-
-document.addEventListener('keydown', function (event) {
-    if (event.keyCode === 13) {
-      window.open("start.html", "_self");
-    }
-});
-
-const playCreditMusic = () => {
-  const audio = new Audio('../sound/sound.mp3');
-  audio.play();
-};
-
-
+//fade in function
 
 const unfade = () => {
     let op = 0.1;  // initial opacity
@@ -19,9 +6,45 @@ const unfade = () => {
         if (op >= 1){
             clearInterval(timer);
         }
-        const element = document.getElementById("body")
+        const element = document.getElementById("body");
         element.style.opacity = op;
         element.style.filter = 'alpha(opacity=' + op * 100 + ")";
         op += op * 0.1;
     }, 50);
 }
+
+
+// fade out function
+
+const fade = () => {
+    let op = 1;  // initial opacity
+    let timer = setInterval(function () {
+        const element = document.getElementById("body");
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
+
+// change page
+
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 13) {
+      // window.open("start.html", "_self");
+
+
+      fade();
+
+      setTimeout(function(){ window.open("start.html", "_self"); }, 1000);
+    }
+});
+
+// const playCreditMusic = () => {
+//   const audio = new Audio('../sound/sound.mp3');
+//   audio.play();
+// };
+
