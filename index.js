185b8669e394f3ab407f9521104794e95faa1fa8
Olivier Girardot <olive_girardot@hotmail.com>
2020-10-12 16:41:08 +0200

    small modification, but I might just drop the lowdb approach completely

--- index.js
+++ index.js
@@ -31,14 +31,6 @@ db.get('players')
 // loading requirejs
 var requirejs = require('requirejs');
 
-requirejs.config({
-    //Pass the top-level main.js/index.js require
-    //function to requirejs so that node modules
-    //are loaded relative to the top-level JS file.
-    nodeRequire: require
-});
-
-
 // load espress
 const app = express();
 
